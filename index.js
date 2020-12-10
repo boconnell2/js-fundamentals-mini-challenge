3b6932107830b56a85fb785b27514c35a08a45d8
boconnell2 <bmoconnell15@gmail.com>
2020-12-10 14:32:16 -0500

    Done

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // ***** Question 1 *****
 // *** Uncomment the lines below to test
 // console.log("%cQuestion 1", "color: red") 
-
+// let counter = 1
+// counter = 2
 // console.log(counter) 
 // // => 2
 // console.log("%c----------", "color: red") 
@@ -12,11 +13,9 @@
 // *** after testing, comment the line causing the error back in 
 // *** otherwise, the error will stop the rest of your code from running
 // console.log("%cQuestion 2", "color: red") 
-
+// const name = 'brendan'
 // console.log(name)
-// => "Raffy"
 // name = "Not Raffy" 
-// => TypeError
 // console.log("%c----------", "color: red") 
 
 
@@ -24,6 +23,13 @@
 // ***** Question 3 *****
 // *** Uncomment the lines below to test
 // console.log("%cQuestion 3", "color: red") 
+
+// function drinkWater(current_thirst_level) {
+//   console.log('Man I sure am thirsty')
+//   current_thirst_level -= 1
+//   console.log('Ahh that hits the spot')
+//   return current_thirst_level
+// }
 
 // console.log(drinkWater(12))
 // // => "Man I sure am thirsty"
@@ -36,6 +42,18 @@
 // ***** Question 4 *****
 // *** Uncomment the lines below to test
 // console.log("%cQuestion 4", "color: red") 
+
+// function sameSameButDifferent(num, maybeNum) {
+//   if (num === maybeNum) {
+//     return 'same same'
+//   }
+//   else if (num == maybeNum) {
+//     return 'same same (but different)'
+//   }
+//   else {
+//     return 'different'
+//   }
+// }
 
 // console.log(sameSameButDifferent(5, 5)) 
 // // => "same same"
@@ -56,6 +74,9 @@
 // *** Uncomment the lines below to test
 // console.log("%cQuestion 5", "color: red")
 // const student1 = { name: "Duane", grade: 88 }
+// function updateGrade(student, grade) {
+//   student1.grade = grade 
+// }
 // updateGrade(student1, 92)
 // console.log(student1)
 // // => { name: "Duane", grade: 92 }
@@ -75,13 +96,22 @@
 //     name: "Liza", phones: { cell: "555-234-5678", office: "555-567-1234" }
 //   }
 // ]
+
+// function printNameAndPhones(user_object) {
+//   user_object.forEach(function(user) {
+//     console.log(user.name)
+//     console.log(`Cell: ${user.phones.cell}`)
+//     console.log(`Office: ${user.phones.office}`)
+//   })
+// }
+
 // printNameAndPhones(users)
 // // => "Duane"
-// => "Cell: 555-123-4567"
-// => "Office: 555-456-7890"
-// => "Liza"
-// => "Cell: 555-234-5678"
-// => "Office: 555-567-1234"
+// // => "Cell: 555-123-4567"
+// // => "Office: 555-456-7890"
+// // => "Liza"
+// // => "Cell: 555-234-5678"
+// // => "Office: 555-567-1234"
 // console.log("%c----------", "color: red") 
 
 
@@ -102,6 +132,10 @@ function myMap(array, callback) {
 // *** Uncomment the lines below to test
 // console.log("%cCallbacks - Question 1", "color: red")
 
+// function triple(value) { 
+//   return value = value * 3
+// }
+
 // console.log(myMap([1,2,3,4,5], triple)) 
 // // => [3,6,9,12,15]
 
@@ -113,6 +147,10 @@ function myMap(array, callback) {
 // ***** Callbacks - Question 2 *****
 // *** Uncomment the lines below to test
 // console.log("%cCallbacks - Question 2", "color: red")
+
+// function greet(name) { 
+//   return `Hi, ${name}`
+// }
 
 // console.log(myMap(["Raffy", "Chase"], greet)) 
 // // => ["Hi, Raffy!", "Hi, Chase!"]
@@ -143,6 +181,14 @@ const line = []
 // *** Uncomment the lines below to test
 // console.log("%cScope & Closures - Question 1", "color: red")
 
+// let counter = 1
+
+// function takeATicketNumber(line) {
+//   line.push(counter)
+//   counter++
+//   return `Welcome. You are ticket ${counter-1}`
+// }
+
 // console.log(takeATicketNumber(line))
 // // => `Welcome. You are ticket number 1`
 
@@ -166,6 +212,17 @@ const line = []
 
 // const newLine = []
 // const takeATicketNumberFunc = ticketNumberGeneratorFunc()
+
+// // incomplete
+// function ticketNumberGeneratorFunc(array) {
+//   if (array.length === 0) {
+//     newLine.push(1)
+//     return `Welcome. You are ticket ${newLine.length}`
+//   } else {
+//     return `Welcome. You are ticket ${newLine.length}`
+//   }
+// }
+
 // console.log(takeATicketNumberFunc(newLine))
 // // => `Welcome. You are ticket number 1`
 
